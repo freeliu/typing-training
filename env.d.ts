32bdc9d94e0780bcf00fbdbf8e6dafef22181347
freeliu <liurc@outlook.com>
2023-07-28 14:39:51 +0800

    fix 添加新vue 文件typescript 报错问题

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+// 解决新文件 Vue: Cannot find module ../views/ET.vue  or its corresponding type declarations.
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
